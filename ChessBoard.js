083739c3c2d9bfebc0adee2374a5419659fea167
f_hein16 <f_hein16@uni-muenster.de>
2022-09-01 11:27:39 +0200

    first commit

---
+++ ChessBoard.js
@@ -0,0 +1,10 @@
+const chessBoard = (size) =>{
+    for (let i = 0; i <= size; i++){
+        console.log(" ");
+        for(let j=0; j <= size; j++){
+            console.log("#");
+             
+        }
+    }
+}
+chessBoard(8);
---
+++ loopingTriangle.js
@@ -0,0 +1,11 @@
+
+let result = '#';
+for(let count =0; count<=7; count=count+1){ 
+    result = result + '#'
+    console.log(result);
+}
+
+
+
+
+
